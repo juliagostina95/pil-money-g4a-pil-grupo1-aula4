5831a462c3f2b634e9e14ef7151d7de148910200
Bruno Protti <brunooprotti@gmail.com>
2021-07-07 19:09:51 -0300

    inicio wallet

--- src/app/wallet/wallet.module.ts
+++ src/app/wallet/wallet.module.ts
@@ -2,15 +2,31 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { WalletPageComponent } from './wallet-page/wallet-page.component';
 import { HeaderWalletComponent } from './header-wallet/header-wallet.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatCardModule } from '@angular/material/card';
+
+
+
 
 
 
 
 
 @NgModule({
-  declarations: [WalletPageComponent, HeaderWalletComponent],
+  declarations: [WalletPageComponent, HeaderWalletComponent, SidebarComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    MatListModule,
+    MatCardModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatButtonModule,
+    MatButtonToggleModule
   ],
   exports: [WalletPageComponent, HeaderWalletComponent]
 })
